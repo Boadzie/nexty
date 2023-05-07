b9cdb0a963c26a07ceb28b9aca94fd382abdae76
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-05-07 20:34:28 +0000

    Add image to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,22 +2,15 @@ import Link from "next/link";
 const Header = () => {
   return (
     <>
-      <header className="text-white bg-slate-600">
-        <ul className="flex items-center justify-between gap-4 p-5 mx-20 text-2xl font-bold ">
-          <div className="flex gap-2">
+      <header className="min-h-[40rem] relative z-0 brightness-[0.4]  bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/efb4855d-e702-43e5-9997-bba0154152e0/35b01c07-9009-43d3-bd56-ee16b48da1c8/GH-en-20230417-popsignuptwoweeks-perspective_alpha_website_large.jpg')] text-white bg-slate-600">
+        <ul className="flex items-center justify-between gap-4 p-5 mx-20 text-2xl font-bold brightness-100 ">
+          <div className="flex gap-2 ">
             <li>
-              <Link className="hover:text-blue-500" href="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:text-blue-500" href="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:text-blue-500" href="/about/team">
-                Team
+              <Link
+                className="text-3xl font-black hover:text-late-100 hover:text-blue-500"
+                href="/"
+              >
+                Nexty
               </Link>
             </li>
           </div>
