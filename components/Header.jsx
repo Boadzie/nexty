d02b1370fc49fcf62b841d4bbf1b4b7e29797be4
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-05-08 10:04:26 +0000

    Fix faint links on navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,21 +2,27 @@ import Link from "next/link";
 const Header = () => {
   return (
     <>
-      <header className="min-h-[40rem] relative z-0 brightness-[0.4]  bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/efb4855d-e702-43e5-9997-bba0154152e0/35b01c07-9009-43d3-bd56-ee16b48da1c8/GH-en-20230417-popsignuptwoweeks-perspective_alpha_website_large.jpg')] text-white bg-slate-600">
-        <ul className="flex items-center justify-between gap-4 p-5 mx-20 text-2xl font-bold brightness-100 ">
-          <div className="flex gap-2 ">
-            <li>
-              <Link
-                className="text-3xl font-black hover:text-late-100 hover:text-blue-500"
-                href="/"
-              >
-                Nexty
-              </Link>
-            </li>
-          </div>
+      <header className="relative">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/efb4855d-e702-43e5-9997-bba0154152e0/35b01c07-9009-43d3-bd56-ee16b48da1c8/GH-en-20230417-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="Your Image"
+          class="w-full h-[40rem]"
+        />
+        <ul class="absolute top-0 left-0 right-0 bottom-0 flex p-4  justify-between items-start bg-black bg-opacity-40 text-white text-opacity-100">
+          <li>
+            <Link
+              className="text-3xl font-black hover:text-late-100 hover:text-blue-500 brightness-125"
+              href="/"
+            >
+              Nexty
+            </Link>
+          </li>
           <div>
             <li>
-              <Link className="hover:text-blue-500" href="/movies">
+              <Link
+                className="hover:text-blue-500 font-bold text-2xl brightness-[10rem]"
+                href="/movies"
+              >
                 Movies
               </Link>
             </li>
