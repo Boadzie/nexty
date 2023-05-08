5dfa6cbf6e05ed5b59767394bbcb558ff0b388da
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-05-08 10:29:37 +0000

    Make banner responsive

--- app/page.jsx
+++ app/page.jsx
@@ -3,23 +3,29 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <section className="mx-[2.5rem]  min-h-[40rem] z-50 w-full absolute top-[10rem]">
-        <h1 className="font-black text-white opacity-100 brightness-100 my-14 text-7xl">
+      <section className="mx-4 lg:mx-[2.5rem]  min-h-[40rem] z-50  absolute top-[5rem] lg:top-[10rem]">
+        <h1 className="w-full mb-4 text-4xl font-black text-white opacity-100 lg:mb-0 brightness-100 my-14 lg:text-7xl">
           Unlimited movies, TV shows, <br /> and more
         </h1>
-        <p className="text-3xl font-bold text-white">
-          Plans now start at US$2.99/month.
-        </p>
-        <p className="mt-2 text-lg font-bold text-white">
-          Ready to watch? Enter your email to create or restart your membership.
-        </p>
-        <form className="flex items-center gap-2 mt-3" action="">
+        <div className="flex flex-col lg:items-start">
+          <p className="text-2xl font-bold text-white lg:text-3xl">
+            Plans now start at US$2.99/month.
+          </p>
+          <p className="mt-2 text-lg font-bold text-white lg:text-left">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+        </div>
+        <form
+          className="flex flex-col items-start gap-2 mt-3 lg:items-center lg:flex-row"
+          action=""
+        >
           <input
-            className="w-[28rem] p-5 bg-transparent rounded-sm ring-slate-400 focus:outline-none ring-1"
+            className="w-[18rem] lg:w-[28rem] p-4 lg:p-5 bg-transparent rounded-sm ring-slate-400 focus:outline-none ring-1"
             type="text"
           />
           <button
-            className="px-4 py-5 text-2xl font-bold text-white bg-red-600 rounded-md"
+            className="px-4 py-3 text-lg font-bold text-white bg-red-600 rounded-md lg:py-5 lg:text-2xl"
             type="submit"
           >
             Get Started
